2e0f1fe10e45d860b57e04035f64db7d34dcf62f
Sofia Larina <zazaq97@gmail.com>
2020-07-02 19:05:08 +0300

    Modified2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 500;
-        int deposit_amount = 1500;
+        int depositAmount = 1500;
         int bonus = 0;
-        if (deposit_amount > 1000) {
-            bonus = deposit_amount / 100;
+        if (depositAmount > 1000) {
+            bonus = depositAmount / 100;
         }
-        int final_balance = balance + deposit_amount + bonus;
+        int finalBalance = balance + depositAmount + bonus;
         System.out.println("Бонус за начисление:" + bonus);
-        System.out.println("Текущий баланс:" + final_balance);
+        System.out.println("Текущий баланс:" + finalBalance);
     }
 }
